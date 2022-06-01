09bef5494b1a100ec2a2d5c0ffe5a8c598741874
coza17 <coza17@outlook.com>
2022-06-01 23:34:30 +0800

    useDragEnd

--- src/pages/Trello/index.tsx
+++ src/pages/Trello/index.tsx
@@ -4,52 +4,10 @@ import AddList from "./components/AddList";
 import CardList from "./components/CardList";
 import Modal from "./components/Modal";
 import styles from "./index.module.less";
+import useDragEnd from "./useDragEnd";
 export default () => {
-  const { state, setState } = store();
-  const handleDragEnd = (result: any) => {
-    //拖拽
-    console.log(result);
-    const { source, destination } = result;
-    if (!destination) {
-      return;
-    } else if (result.type === "card") {
-      //卡片拖拽
-      if (source.droppableId === destination.droppableId) {
-        //同列表
-        const newList = JSON.parse(JSON.stringify(state));
-        newList.forEach((listItem: any) => {
-          if (listItem.id.toString() === destination.droppableId) {
-            const item = listItem.cards.splice(source.index, 1);
-            listItem.cards.splice(destination.index, 0, item[0]);
-          }
-        });
-        setState(newList);
-      } else if (source.droppableId !== destination.droppableId) {
-        //跨列表
-        const newList = JSON.parse(JSON.stringify(state));
-        newList.forEach((listItem: any) => {
-          if (listItem.id.toString() === source.droppableId) {
-            //删除、保存
-            const item = listItem.cards.splice(source.index, 1);
-            newList.forEach((nList: any) => {
-              if (nList.id.toString() === destination.droppableId) {
-                //添加
-                nList.cards.splice(destination.index, 0, item[0]);
-              }
-            });
-          }
-        });
-        setState(newList);
-      }
-    } else if (result.type === "list") {
-      // 列表拖拽
-      const newList = JSON.parse(JSON.stringify(state));
-      const item = newList.splice(source.index, 1);
-      newList.splice(destination.index, 0, item[0]);
-      setState(newList);
-    }
-  };
-
+  const { state } = store();
+  const {handleDragEnd}=useDragEnd();
   return (
     <>
       <div className={styles.all}>
---
+++ src/pages/Trello/useDragEnd.tsx
@@ -0,0 +1,50 @@
+import store from "@/models/store";
+
+const useDragEnd =()=>{
+    const { state, setState } = store();
+    const handleDragEnd = (result: any) => {
+      //拖拽
+      console.log(result);
+      const { source, destination } = result;
+      if (!destination) {
+        return;
+      } else if (result.type === "card") {
+        //卡片拖拽
+        if (source.droppableId === destination.droppableId) {
+          //同列表
+          const newList = JSON.parse(JSON.stringify(state));
+          newList.forEach((listItem: any) => {
+            if (listItem.id.toString() === destination.droppableId) {
+              const item = listItem.cards.splice(source.index, 1);
+              listItem.cards.splice(destination.index, 0, item[0]);
+            }
+          });
+          setState(newList);
+        } else if (source.droppableId !== destination.droppableId) {
+          //跨列表
+          const newList = JSON.parse(JSON.stringify(state));
+          newList.forEach((listItem: any) => {
+            if (listItem.id.toString() === source.droppableId) {
+              //删除、保存
+              const item = listItem.cards.splice(source.index, 1);
+              newList.forEach((nList: any) => {
+                if (nList.id.toString() === destination.droppableId) {
+                  //添加
+                  nList.cards.splice(destination.index, 0, item[0]);
+                }
+              });
+            }
+          });
+          setState(newList);
+        }
+      } else if (result.type === "list") {
+        // 列表拖拽
+        const newList = JSON.parse(JSON.stringify(state));
+        const item = newList.splice(source.index, 1);
+        newList.splice(destination.index, 0, item[0]);
+        setState(newList);
+      }
+    };
+    return {handleDragEnd}
+}
+export default useDragEnd
